69f359f238624b0f058b7eaf9d21d98ee04b2c03
jean4599@ <jean4599@gmail.com>
2017-05-14 16:50:25 +0900

    post question ui and function

--- public/post.js
+++ public/post.js
@@ -5,11 +5,11 @@ $( document ).ready(function() {
 
 	$('#post-btn').on('click',function(){
 		$('#ask').toggleClass('hide');
-		$('#posts').toggleClass('hide');
+		$('#overview').toggleClass('hide');
 	})
 	$('#cancel-post-btn').on('click', function(){
 		$('#ask').toggleClass('hide');
-		$('#posts').toggleClass('hide');
+		$('#overview').toggleClass('hide');
 	})
 	$('#submit-post-btn').on('click', function(){
 		const question = document.getElementById('question').value;
@@ -26,7 +26,9 @@ $( document ).ready(function() {
 		savePost(post);
 
 		$('#ask').toggleClass('hide');
-		$('#posts').toggleClass('hide');
+		$('#overview').toggleClass('hide');
+		document.getElementById('question').value='';
+		document.getElementById('discription').value='';
 	})
 
 	function savePost(post){
@@ -39,23 +41,27 @@ $( document ).ready(function() {
 		firebase.database().ref('/posts/').on('value', function(snapshot){
 			var result = snapshot.val();
 			if(result){
+				clearAllPost();
 				posts = Object.keys(result).map(key =>{
-					result.key['key'] = key;
-					renderPost(result.key)
+					result[key].key = key;
+					renderPost(result[key])
 					return result.key;
 				})
 			}
 		})
 	}
+	function clearAllPost(){
+		$('#posts').children().remove();
+	}
 	function renderPost(post){
-		var newPost = "<div class='panel panel-default'>
-		          <div class='panel-heading'>
-		            <h3 class='panel-title'>"+post.question+"</h3>
-		          </div>
-		          <div class='panel-body'>"+
+		var newPost = "<div class='panel panel-default'>"+
+		          "<div class='panel-heading'>"+
+		            "<h3 class='panel-title'>"+post.question+
+		          "</h3></div>"+
+		          "<div class='panel-body'>"+
 		            post.discription+
-		          "</div>
-		        </div>";
+		          "</div></div>";
+		$('#posts').append(newPost);
 	}
 	function getTimeStamp(){
 		var currentdate = new Date(); 
