12d99d63508adc505a81b8960e9189c0adc10e66
jongminpark <qwaqwa93@kaist.ac.kr>
2017-05-18 13:16:27 +0900

    Caturing video screen2

--- public/capture.js
+++ public/capture.js
@@ -1,48 +1,52 @@
-var vid = document.getElementById("myVideo");
+$( document ).ready(function() {
+  var vid = document.getElementById("myVideo");
 
-// Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
-vid.ontimeupdate = function() {myFunction()};
+  // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
+  vid.ontimeupdate = function() {myFunction()};
 
-function myFunction() {
-// Display the current position of the video in a p element with id="demo"
-document.getElementById("demo").innerHTML = vid.currentTime;
-}
+  document.getElementById("")
 
-function favorite_click() {
-  var favorite_button = document.getElementById("favorite_button");
-
-  if (favorite_button.src.indexOf("assets/Favorite_before.png") != -1) {
-    favorite_button.src="assets/Favorite_after.png";
-    alert("Added to your favorite list");
+  function myFunction() {
+  // Display the current position of the video in a p element with id="demo"
+  document.getElementById("demo").innerHTML = vid.currentTime;
   }
-  else {
-    favorite_button.src="assets/Favorite_before.png";
-    alert("Removed from your favorite list")
+/*
+  function favorite_click() {
+    var favorite_button = document.getElementById("favorite_button");
+
+    if (favorite_button.src.indexOf("assets/Favorite_before.png") != -1) {
+      favorite_button.src="assets/Favorite_after.png";
+      alert("Added to your favorite list");
+    }
+    else {
+      favorite_button.src="assets/Favorite_before.png";
+      alert("Removed from your favorite list")
+    }
   }
-}
+*/
+  function post_question_click() {
+    var screenshot;
+    capture_screenshot();
+  }
 
-function post_question_click() {
-  var screenshot;
-  capture_screenshot();
-}
+  function capture_screenshot() {
+    // Capture current screen of current video and save it as image file. Return the address of the images
+    var video = document.getElementById("myVideo");
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
 
-function capture_screenshot() {
-  // Capture current screen of current video and save it as image file. Return the address of the images
-  var video = document.getElementById("myVideo");
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+    canvas.width = 600;
+    canvas.height = 400;
 
-  canvas.width = 600;
-  canvas.height = 400;
+    draw(this, context, 600, 400);
 
-  draw(this, context, 600, 400);
+    alert("clicked");
+            
+    //var data = canvas.toDataURL('image/png');
+    //document.write('<img src="'+data+'"/>');
+  }
 
-  alert("clicked");
-          
-  //var data = canvas.toDataURL('image/png');
-  //document.write('<img src="'+data+'"/>');
-}
-
-function draw(v, c, w, h) {
-  c.drawImage(v, 0, 0, w, ,h);
+  function draw(v, c, w, h) {
+    c.drawImage(v, 0, 0, w, ,h);
+  }
 }
