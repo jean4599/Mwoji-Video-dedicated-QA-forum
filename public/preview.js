35c74a135f78982e6fc24b32af8cbb6666d5ee14
Kamil Veli TORAMAN <kvtoraman@gmail.com>
2017-05-20 21:30:15 +0900

    Feedback.js, information pic,

--- public/preview.js
+++ public/preview.js
@@ -3,6 +3,15 @@ var url;
 var ref;
 var fn;
 $(document).ready(function(){
+	$(".sidebox-image").click(function(){
+		alert("it worksss!");
+	})
+	$(document).on('mouseenter', '.sidebox-image', function() {
+   		$('html,css').css('cursor','pointer');
+	});
+	$(document).on('mouseleave', '.sidebox-image', function() {
+		$('html,css').css('cursor','default');
+	});
 	function initPopular(){
 		var fileName;
 		var ref = firebase.database().ref("/videos/");
