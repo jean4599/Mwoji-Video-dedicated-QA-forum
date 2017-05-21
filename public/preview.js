e45b5c6e00dfbf1328f604b04d7f1b40c297e6f9
Kamil Veli TORAMAN <kvtoraman@gmail.com>
2017-05-21 10:44:16 +0900

    Buf fix in preview

--- public/preview.js
+++ public/preview.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 						}
 						$(".sb-video-log").first().before('<div class="sb-vdo-list"">\
 							<div class="sb-vdo-thumb">\
-							<a href="" ><span><img src="images/'+current.fileName+'.jpg" width=80 height=64></img>'+current.length+'</span></a>\
+							<a href="preview.html?fileName='+ current.fileName +'" ><span><img src="images/'+current.fileName+'.jpg" width=80 height=64></img>'+current.length+'</span></a>\
 		                	</div>\
 		                	<div class=\"sb-vdo-info\">\
 		                    <div class=\"sb-vdo-title\"><a href="preview.html?fileName='+ current.fileName +'"><h3>'+current.title+'<\/h3><\/a><\/div>\
