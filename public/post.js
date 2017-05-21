1ab55646900a17505c616b6407d5c34a4614486c
jean4599@ <jean4599@gmail.com>
2017-05-21 17:09:29 +0900

    attach screenshot to video

--- public/post.js
+++ public/post.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 	var posts=[];
-
+	const videoName = 'video2';
+	const REF_question = 'videos/videoList/'+videoName+'/questions';
 	initPost();
 
 	$('#post-btn').on('click',function(){
@@ -14,11 +15,13 @@ $( document ).ready(function() {
 	$('#submit-post-btn').on('click', function(){
 		const question = document.getElementById('question').value;
 		const discription = document.getElementById('discription').value;
+		const videoTime = document.getElementById("myVideo").currentTime;
 		const timestamp = getTimeStamp()
 		const post = {
 			question: question,
 			discription: discription,
-			time: timestamp,
+			postTime: timestamp,
+			videoTime: videoTime,
 		}
 		console.log('New Post:')
 		console.log(post)
@@ -34,34 +37,43 @@ $( document ).ready(function() {
 	function savePost(post){
 		console.log('Firebase save: ')
 		console.log(post)
-		firebase.database().ref('/posts/').push(post)
+		firebase.database().ref(REF_question).push(post)
 	}
 
 	function initPost(){
-		firebase.database().ref('/posts/').on('value', function(snapshot){
+		firebase.database().ref(REF_question).on('value', function(snapshot){
 			var result = snapshot.val();
 			if(result){
 				clearAllPost();
+				clearAllScreenshot();
 				posts = Object.keys(result).map(key =>{
 					result[key].key = key;
 					renderPost(result[key])
-					return result.key;
+					return result[key];
 				})
+				console.log(posts)
+				getVideoScreenShot(posts, posts.length-1)
 			}
 		})
+	}
+	function clearAllScreenshot() {
+		$('#testposition').children().remove();
 	}
 	function clearAllPost(){
 		$('#posts').children().remove();
 	}
 	function renderPost(post){
-		var newPost = "<div class='panel panel-default'>"+
+
+		var newPost = "<div class='panel panel-default' id='"+post.key+"'>"+
 		          "<div class='panel-heading'>"+
 		            "<h3 class='panel-title'>"+post.question+
 		          "</h3></div>"+
 		          "<div class='panel-body'>"+
 		            post.discription+
 		          "</div></div>";
+		
 		$('#posts').append(newPost);
+		
 	}
 	function getTimeStamp(){
 		var currentdate = new Date(); 
@@ -72,5 +84,30 @@ $( document ).ready(function() {
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
         return datetime;
+	}
+	function getVideoScreenShot(posts, i, canvas_id="myCanvas", width=200, height=150){
+		if (i < 0) {
+			return;
+		}
+		var video = document.getElementById("myVideo");
+		var cnvs;
+		var curtime = video.currentTime;
+
+		video.currentTime = posts[i].videoTime;
+		video.onseeked = function() {
+		var cnvs = document.createElement("canvas");
+		var context = cnvs.getContext("2d");
+		context.drawImage(video, 0, 0, 220, 150);
+		cnvs.id = canvas_id
+		cnvs.style.width = width;
+		cnvs.style.height = height;
+
+		// Append "cnvs" into the html (where you want)
+		document.getElementById(posts[i].key).appendChild(cnvs);
+
+		video.currentTime = curtime;
+		video.onseeked = null;
+		getVideoScreenShot(posts, i-1)
+		}
 	}
 })
